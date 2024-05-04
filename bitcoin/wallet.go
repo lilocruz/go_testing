3aee6c21609c35f62977c64e0431b1a448c9dabc
Michael Sanchez <superlinux.michael5@gmail.com>
2024-05-03 22:19:31 -0400

    Adding the BTC and wallet

---
+++ bitcoin/wallet.go
@@ -0,0 +1,30 @@
+package bitcoin
+
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+type Wallet struct{
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	(*w).balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return (*w).balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	(*w).balance -= amount
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
